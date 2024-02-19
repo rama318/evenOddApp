614803f20ec4da1f920e268cb43b10220849167a
Rama Krishna Gudla <gudlaramakrishna355@gmail.com>
2024-02-19 12:47:07 +0000

    adding the even and odd count of the count

---
+++ src/components/EvenOddApp/index.js
@@ -0,0 +1,40 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {count: 0}
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
+
+  onIncrement = () => {
+    const randomNumber = this.getRandomNumber()
+
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const displayText = count % 2 === 0 ? 'even' : 'odd'
+    return (
+      <div className="app-container">
+        <div className="container">
+          <h1 className="heading">count {count}</h1>
+          <p className="description">count is {displayText}</p>
+          <button
+            type="button"
+            className="increment-button"
+            onClick={this.onIncrement}
+          >
+            Increment
+          </button>
+          <p className="description">
+            *Increase By Random Number Between 0 to 100
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
